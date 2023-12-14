9cbdad8a54c294a8c8aa10bdfd1ddbe902a7ca68
muddabir chohan <muddabir22@gmail.com>
2023-12-14 23:17:41 +0500

    added characters page with basic style vuetify (#1)

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,21 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'vuetify/styles/main.css';
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
-app.use(router)
+createApp(App).use(vuetify).mount('#app')
+// const app = createApp(App)
 
-app.mount('#app')
+// app.use(router)
+// app.use(vuetify)
+
+// app.mount('#app')
